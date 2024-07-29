008062b5c2a22bbe944256b741bbc15b639ae161
YJ Shieh <yoman8117@gmail.com>
2024-07-29 21:31:24 +0800

    refined version and add avg word count

--- reviews_analytics.py
+++ reviews_analytics.py
@@ -7,5 +7,10 @@ with open('reviews.txt', 'r') as f:
 		if count % 1000 == 0:
 			print(len(data))
 
-print(len(data)) #review count
-print(data[0])
+print('File read. There are ', len(data), 'reviews in total') 
+
+#average word count for each review
+sum_len = 0
+for d in data:
+	sum_len = sum_len + len(d)
+print('The average word count is', sum_len/len(data), 'per review.')
